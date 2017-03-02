146a49210606bbab7b4baf1f454f5f41308612f7
Jeet Patel <jeet.patel1022@gmail.com>
2017-03-02 17:04:21 -0500

    Removed Sequitur-Swift dynamic

--- Sequitur-Swift-Example/Sequitur-Swift-Example/main.swift
+++ Sequitur-Swift-Example/Sequitur-Swift-Example/main.swift
@@ -6,12 +6,7 @@
 //  Copyright © 2017 Jeet. All rights reserved.
 //
 
-import Foundation
-
 print("Welcome to Sequitur for Swift!\n");
 print("Reading from: \(CommandLine.arguments[1])")
-
-Sequitur(str: CommandLine.arguments[1])
-
 var a = 5
 
